8cd3dda59b1ebbb0a9949e90f77070f5abea8e80
divyanshu <divyanshu.srivastava@mobisy.com>
2019-01-21 05:06:17 +0530

    fix: changed production endpoint

--- src/screens/upload/constants.js
+++ src/screens/upload/constants.js
@@ -1,2 +1,2 @@
-export const endpoint = 'http://localhost:8000/upload';
+export const endpoint = 'http://52.14.131.151:8000/upload';
 export const cloudFrontEndPoint = 'https://d3ne5stkes596s.cloudfront.net'
